2a68ffdab64b1ee11c10de10bfdcbf333c6defe8
khangltt2003 <khangltt2003@gmail.com>
2023-07-24 18:47:25 -0500

    refactor

--- 3. Popup Modal/script.js
+++ 3. Popup Modal/script.js
@@ -1,22 +1,22 @@
 const openButton = document.querySelector(".openButton");
-const closeButtons = document.querySelectorAll(".closeButton")
+const closeButtons = document.querySelectorAll(".closeButton");
 const popup = document.querySelector(".popup-container");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
-function togglePopUp(){
-    popup.classList.toggle("hidden");
+function togglePopUp() {
+  popup.classList.toggle("hidden");
 }
 
-openButton.addEventListener("click", togglePopUp)
+openButton.addEventListener("click", togglePopUp);
 
-popup.addEventListener("click", (e)=>{
-    //e.target = clicked element
-    //e.current = element that eventHandler attached to (modal)
-    if(e.target === e.currentTarget){
-        togglePopUp();
-    }
-})
+popup.addEventListener("click", (e) => {
+  //e.target = clicked element
+  //e.current = element that eventHandler attached to (modal)
+  if (e.target === e.currentTarget) {
+    togglePopUp();
+  }
+});
 
-closeButtons.forEach((button)=>{
-    button.addEventListener("click", togglePopUp)
-})
+closeButtons.forEach((button) => {
+  button.addEventListener("click", togglePopUp);
+});
--- 4. Image Gallery/script.js
+++ 4. Image Gallery/script.js
@@ -1,66 +1,73 @@
 const images = document.querySelectorAll(".image img");
-const nextButton = document.querySelector('.next')
-const previousButton = document.querySelector('.previous')
-const galleryImage = document.querySelector(".gallery-img img")
-const gallery = document.querySelector('.gallery');
+const nextButton = document.querySelector(".next");
+const previousButton = document.querySelector(".previous");
+const galleryImage = document.querySelector(".gallery-img img");
+const gallery = document.querySelector(".gallery");
 const closeButton = document.querySelector(".closeButton");
-const imgArr = ['dalat', 'danang', 'hagiang', 'halongbay','ninhbinh', 'sapa','phongnha', 'hoian']
+const imgArr = [
+  "dalat",
+  "danang",
+  "hagiang",
+  "halongbay",
+  "ninhbinh",
+  "sapa",
+  "phongnha",
+  "hoian",
+];
 
 let currentIndex = 0;
 
-function toggleGallery(){
-    gallery.classList.toggle('show');
+function toggleGallery() {
+  gallery.classList.toggle("show");
 }
 
-function renderImage(index){
-    let imageSource = "/4. Image Gallery/public/images/" + imgArr[index] + ".png"
-    galleryImage.setAttribute('src', imageSource)
+function renderImage(index) {
+  let imageSource = "/4. Image Gallery/public/images/" + imgArr[index] + ".png";
+  galleryImage.setAttribute("src", imageSource);
 }
 
-function nextImage(){
-    currentIndex +=1;
-    if(currentIndex > imgArr.length - 1){
-        currentIndex = 0;
-    }
+function nextImage() {
+  currentIndex += 1;
+  if (currentIndex > imgArr.length - 1) {
+    currentIndex = 0;
+  }
+  renderImage(currentIndex);
+}
+
+function previousImage() {
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  renderImage(currentIndex);
+}
+
+images.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    currentIndex = index;
     renderImage(currentIndex);
-}
+    toggleGallery();
+  });
+});
 
-function previousImage(){
-    currentIndex -= 1;
-    if(currentIndex < 0){
-        currentIndex = images.length-1;
-    }
-    renderImage(currentIndex);
-}
+previousButton.addEventListener("click", () => {
+  previousImage();
+});
 
-images.forEach((image, index)=>{
-    image.addEventListener('click', ()=>{
-        currentIndex = index
-        renderImage(currentIndex);
-        toggleGallery();
-    })
-})
-
-previousButton.addEventListener('click', ()=>{
-    previousImage();
-})
-
-nextButton.addEventListener('click', ()=>{
-    nextImage()
-})
+nextButton.addEventListener("click", () => {
+  nextImage();
+});
 
 closeButton.addEventListener("click", toggleGallery);
 
-document.querySelector("body").addEventListener('keydown',(e)=>{
-    if(gallery.classList.contains("show")){
-        if(e.key === "Escape"){
-            toggleGallery();
-        }
-        else if(e.key === "ArrowLeft"){
-            previousImage();
-        }
-        else if(e.key === "ArrowRight"){
-            nextImage();
-        }
+document.querySelector("body").addEventListener("keydown", (e) => {
+  if (gallery.classList.contains("show")) {
+    if (e.key === "Escape") {
+      toggleGallery();
+    } else if (e.key === "ArrowLeft") {
+      previousImage();
+    } else if (e.key === "ArrowRight") {
+      nextImage();
     }
-})
+  }
+});
