a89e2b91b226bb47fc13ff41b18f9c0ac6e6bc09
khangltt2003 <khangltt2003@gmail.com>
2023-07-24 16:37:20 -0500

    refactor

---
+++ 6. Keydown/script.js
@@ -0,0 +1,12 @@
+function renderPage(e) {
+  document.querySelector(".key-code").innerHTML = e.keyCode;
+  document.querySelector(".key .bottom").innerHTML = e.key;
+  document.querySelector(".location .bottom").innerHTML = e.location;
+  document.querySelector(".keycode .bottom").innerHTML = e.keyCode;
+  document.querySelector(".code .bottom").innerHTML = e.code;
+}
+
+document.addEventListener("keydown", (e) => {
+  console.log(e);
+  renderPage(e);
+});
