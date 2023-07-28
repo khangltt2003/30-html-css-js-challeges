fa8ef6aeb822f9bbbb6dfa9e560a3d0c2072e049
khangltt2003 <khangltt2003@gmail.com>
2023-07-27 19:49:57 -0500

    refactor

---
+++ 9. Weather App/script.js
@@ -0,0 +1,29 @@
+import apiKey from "./apikey.js";
+const input = document.querySelector("input");
+const searchButton = document.querySelector(".search-box i");
+const place = document.querySelector(".location");
+const time = document.querySelector(".time");
+const temperature = document.querySelector(".temperature");
+const weather = document.querySelector(".weather");
+const vision = document.querySelector(".vision p");
+const humidty = document.querySelector(".humidity p");
+const windSpeed = document.querySelector(".wind-speed p");
+
+async function renderUI(city) {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const data = await fetch(URL).then((res) => res.json());
+  console.log(data);
+  if (data.cod !== "404") {
+    place.innerText = data.name;
+    time.innerText = new Date().toLocaleString();
+    temperature.innerText = data.main.temp + " C";
+    weather.innerText = data.weather[0].main;
+    vision.innerText = data.visibility + " m";
+    humidty.innerText = data.main.humidity + "km/h";
+    windSpeed.innerText = data.wind.speed + "%";
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  renderUI(input.value);
+});
