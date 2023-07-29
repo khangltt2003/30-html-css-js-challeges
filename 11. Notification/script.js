5d90fa601705c9f6ac8ace9e6fd26b22a2ce818e
khangltt2003 <khangltt2003@gmail.com>
2023-07-29 18:53:02 -0500

    add script

---
+++ 11. Notification/script.js
@@ -0,0 +1,27 @@
+const successButton = document.querySelector(".success-button");
+const warningButton = document.querySelector(".warning-button");
+const errorButton = document.querySelector(".error-button");
+const messageBox = document.querySelector(".message");
+const messageList = document.querySelector(".message-list");
+
+function showMessage(message, type) {
+  var messageElement = document.createElement(`div`);
+  messageElement.classList.add(type);
+  messageElement.innerHTML = message;
+  messageList.prepend(messageElement);
+  setTimeout(() => {
+    messageList.removeChild(messageElement);
+  }, 7000);
+}
+
+successButton.addEventListener("click", () => {
+  showMessage("Success Message !!!", "success");
+});
+
+warningButton.addEventListener("click", () => {
+  showMessage("Warning Message !!!", "warning");
+});
+
+errorButton.addEventListener("click", () => {
+  showMessage("Error Message !!!", "error");
+});
