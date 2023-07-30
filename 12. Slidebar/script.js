2b1a204d63b61166873619db4fc2d61482882ad2
khangltt2003 <khangltt2003@gmail.com>
2023-07-30 18:38:20 -0500

    refactor

--- 12. Slidebar/script.js
+++ 12. Slidebar/script.js
@@ -1,8 +1,26 @@
-const slider = document.querySelector("input");
-const blur = document.querySelector(".blur");
-console.log(slider.value);
+const slider = document.querySelector(".slider");
+const process = document.querySelector(".process");
+const thumb = document.querySelector(".thumb");
+const blurBackground = document.querySelector(".blur");
 
-slider.addEventListener("input", () => {
-  console.log(slider.value);
-  blur.style.backgroundColor = `rgba(0, 0, 0, ${slider.value / 120})`;
+let onHold = false;
+
+thumb.addEventListener("mousedown", () => {
+  onHold = true;
+});
+
+document.addEventListener("mouseup", () => {
+  onHold = false;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (onHold) {
+    let processWidth = e.pageX - slider.offsetLeft;
+    let percent = Math.round((processWidth / slider.offsetWidth) * 100);
+    if (percent <= 100 && percent >= 0) {
+      process.style.width = percent + "%";
+      process.innerText = percent + "%";
+      blurBackground.style.backgroundColor = `rgba(0, 0, 0, ${percent / 120})`;
+    }
+  }
 });
