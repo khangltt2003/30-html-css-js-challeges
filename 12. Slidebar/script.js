17548ead4113591a752aaaa4ce3b50afda330810
khangltt2003 <khangltt2003@gmail.com>
2023-07-30 18:49:19 -0500

    refactor

--- 12. Slidebar/script.js
+++ 12. Slidebar/script.js
@@ -15,12 +15,15 @@ document.addEventListener("mouseup", () => {
 
 document.addEventListener("mousemove", (e) => {
   if (onHold) {
+    //pageX : distance from mouse to left edge of browser
+    //offsetLeft: distance from left edge of element to left edge of browser
+    //offsetWidth: width of element
     let processWidth = e.pageX - slider.offsetLeft;
     let percent = Math.round((processWidth / slider.offsetWidth) * 100);
-    if (percent <= 100 && percent >= 0) {
+    if (0 <= percent && percent <= 100) {
       process.style.width = percent + "%";
       process.innerText = percent + "%";
-      blurBackground.style.backgroundColor = `rgba(0, 0, 0, ${percent / 120})`;
+      blurBackground.style.backgroundColor = `rgba(0, 0, 0, ${percent / 100})`;
     }
   }
 });
