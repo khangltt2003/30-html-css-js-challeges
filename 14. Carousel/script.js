62bffbc135e4c552ffb56d5ad585661952929c67
khangltt2003 <khangltt2003@gmail.com>
2023-08-03 00:00:33 -0500

    refactor

--- 14. Carousel/script.js
+++ 14. Carousel/script.js
@@ -9,48 +9,45 @@ let currentIndex = 0;
 mainImg.setAttribute("src", "./images/danang.png");
 
 //update UI with new element passed;
-function updateUI(newImg) {
+function updateUI(index) {
   lastImg.classList.remove("highlight"); //remove highlight of prev img
-  newImg.classList.add("highlight"); //add highlight to curr img
-  mainImg.src = newImg.querySelector("img").src; //set main img src to src curr img src
-  lastImg = newImg; //set last img to current img
+  imgList[index].classList.add("highlight"); //add highlight to curr img
+  mainImg.src = imgList[index].querySelector("img").src; //set main img src to src curr img src
+  lastImg = imgList[index]; //set last img to current img
+}
+
+function slideTransition(newIndex, currentIndex) {
+  //side left when new image's index > current img's index
+  if (newIndex > currentIndex) {
+    mainImg.style.animation = "slideLeftOut 0.2s forwards";
+    setTimeout(() => {
+      updateUI(newIndex);
+      mainImg.style.animation = "slideLeftIn 0.2s forwards";
+    }, 200);
+  }
+  //side right when new image's index < current img's index
+  else if (newIndex < currentIndex) {
+    mainImg.style.animation = "slideRightOut 0.2s forwards";
+    setTimeout(() => {
+      updateUI(newIndex);
+      mainImg.style.animation = "slideRightIn 0.2s forwards";
+    }, 200);
+  }
 }
 
 imgList.forEach((img, index) => {
   img.addEventListener("click", (e) => {
-    if (index > currentIndex) {
-      mainImg.style.animation = "slideLeftOut 0.2s forwards";
-      setTimeout(() => {
-        updateUI(img);
-        mainImg.style.animation = "slideLeftIn 0.2s forwards";
-      }, 200);
-    } else if (index < currentIndex) {
-      mainImg.style.animation = "slideRightOut 0.2s forwards";
-      setTimeout(() => {
-        updateUI(img);
-        mainImg.style.animation = "slideRightIn 0.2s forwards";
-      }, 200);
-    }
+    slideTransition(index, currentIndex);
     currentIndex = index; //assign current index with index
   });
 });
 
 prevButton.addEventListener("click", () => {
-  currentIndex--;
   if (currentIndex < 0) currentIndex = imgList.length - 1;
-  mainImg.style.animation = "slideLeftOut 0.2s forwards";
-  setTimeout(() => {
-    updateUI(imgList[currentIndex]);
-    mainImg.style.animation = "slideLeftIn 0.2s forwards";
-  }, 200);
+  slideTransition(currentIndex--, currentIndex);
 });
 
 nextButton.addEventListener("click", () => {
-  currentIndex++;
   if (currentIndex > imgList.length - 1) currentIndex = 0;
-  mainImg.style.animation = "slideRightOut 0.2s forwards";
-  setTimeout(() => {
-    updateUI(imgList[currentIndex]);
-    mainImg.style.animation = "slideRightIn 0.2s forwards";
-  }, 200);
+  slideTransition(currentIndex++, currentIndex);
 });
